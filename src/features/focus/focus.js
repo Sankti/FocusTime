72cb93e0f1916666b1285e854343dfba8a6ba730
Adam <sanktimarus@gmail.com>
2021-02-17 20:03:57 +0100

    Using utils to provide generic style variables to components

--- src/features/focus/focus.js
+++ src/features/focus/focus.js
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
 import { RoundedButton } from '../../components/RoundedButton';
+import { fontSizes, spacing } from '../../utils/sizes';
 
 export const Focus = ({ addSubject }) => {
   const [tmpItem, setTmpItem] = useState(null)
@@ -12,7 +13,7 @@ export const Focus = ({ addSubject }) => {
         <Text style={styles.title}>What would you like to focus on?</Text>
         <View style={styles.inputContainer}>
           <TextInput
-            style={{ flex: 1, paddingRight: 20, marginRight: 20 }}
+            style={{ flex: 1, marginRight: spacing.medium }}
             onSubmitEditing={
               ({ nativeEvent }) => {
                 setTmpItem(nativeEvent.text)
@@ -31,16 +32,16 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     flex: 0.5,
-    padding: 16,
+    padding: spacing.medium,
     justifyContent: 'center',
   },
   title: {
     color: 'white',
     fontWeight: 'bold',
-    fontSize: '24',
+    fontSize: fontSizes.large,
   },
   inputContainer: {
-    paddingTop: 20,
+    paddingTop: spacing.medium,
     flexDirection: 'row',
     alignItems: 'center'
   },
--- src/utils/sizes.js
+++ src/utils/sizes.js
@@ -1,8 +1,17 @@
 export const fontSizes = {
-  sm: 8,
-  md: 16,
-  lg: 24,
+  small: 8,
+  medium: 16,
+  large: 24,
   xl: 32,
   xxl: 40,
   xxxl: 80
-}
+};
+
+export const spacing = {
+  small: 8,
+  medium: 16,
+  large: 24,
+  xl: 32,
+  xxl: 40,
+  xxxl: 80
+};
