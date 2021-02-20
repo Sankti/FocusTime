2c12f4bcfd22c7a8e73a6f263517b6116cb42a81
Adam <sanktimarus@gmail.com>
2021-02-20 12:40:02 +0100

    Setting up file for the Countdown component

---
+++ src/components/Countdown.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export const Countdown = () => {
+  return (
+    <Text>Countdown timer goes here.</Text>
+  )
+};
